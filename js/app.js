c9bf9683f84ee51e95dc7f4a78427888a2859e8d
Schettino <edu.schettino98@gmail.com>
2022-11-25 03:05:33 -0300

    Se agrega uso de  localstorage

--- js/app.js
+++ js/app.js
@@ -1,49 +1,88 @@
-let addbnt = document.querySelector(".addbtn")
+const btnAdd = document.querySelector(".addbtn")
+const list = document.querySelector(".lista")
 
-addbnt.onclick = function () {
-    let input = document.querySelector("#entrada").value
-    if (input.length == 0) {
-        alert("Please Enter a Task")
-    }
-    else {
-        let datos = `
-        <div class="item-list">
-        <li>${input}</li>
-        <span class="readybtn"><i class="fa-solid fa-check"></i></span>
-        <span class="deletebtn"><i class="fa-solid fa-trash"></i></span>
-        </div>`
-        document.querySelector(".lista").innerHTML += datos
-        document.querySelector("#entrada").value = ""
 
-        if (document.querySelectorAll(".readybtn").length > 0) {
-            let tareas = document.querySelectorAll(".readybtn")
-            let borrar = document.querySelectorAll(".deletebtn")
-            tareas.forEach((e)=>{
-                e.addEventListener('click', ()=>{
-                    e.parentElement.className= "ready"
-                })
-            })
-            borrar.forEach((e)=>{
-                e.addEventListener('click',()=>{
-                    e.parentElement.remove()
-                })
-            })
+const task = JSON.parse(localStorage.getItem("tareas")) || []
+const taskComplete = JSON.parse(localStorage.getItem("TareasComplete")) || []
+
+
+
+const add = () => {
+    btnAdd.addEventListener('click', () => {
+        const input = document.querySelector("#entrada")
+        const valor = input.value
+        if (valor.length == 0 || valor.length < 3) {
+            alert("Ingresa algo po xuxetumare")
         }
-
-
-        // if (document.querySelectorAll(".readybtn").length > 0) {
-        //     let tareas = document.querySelectorAll(".readybtn")
-        //     let borrar = document.querySelectorAll(".deletebtn")
-        //     for (let i = 0; i < tareas.length; i++) {
-        //         tareas[i].onclick = function () {
-        //             console.log(tareas[i].parentElement)
-        //             tareas[i].parentElement.className = "ready"
-        //         }
-        //         borrar[i].onclick = function(){
-        //             borrar[i].parentElement.remove()
-        //         }
-        //     }
-        // }
-
-    }
+        else {
+            let datos = `<div class="item-list"><li class="data">${input.value}</li><span class="readybtn"><i class="fa-solid fa-check"></i></span><span class="deletebtn"><i class="fa-solid fa-trash"></i></span></div>`
+            list.innerHTML += datos
+            input.value = ""
+            task.push(valor)
+            localStorage.setItem("tareas", JSON.stringify(task))
+            location.reload()
+        }
+    })
 }
+
+
+const loadTask = () => {
+    task.forEach((e) => {
+        let datos = `<div class="item-list"><li class="data">${e}</li><span class="readybtn"><i class="fa-solid fa-check"></i></span><span class="deletebtn"><i class="fa-solid fa-trash"></i></span></div>`
+        list.innerHTML += datos
+    })
+    taskComplete.forEach((i) => {
+        let datos = `<div class="ready"><li class="data">${i}</li><span class="readybtn"><i class="fa-solid fa-check"></i></span><span class="deletebtn"><i class="fa-solid fa-trash"></i></span></div>`
+        list.innerHTML += datos
+    })
+}
+
+
+
+const tasksComplete = () => {
+    const btnComplete = document.querySelectorAll(".readybtn")
+    btnComplete.forEach((e) => {
+        e.addEventListener('click', () => {
+            e.parentElement.className = "ready"
+            const busqueda = task.indexOf(e.parentElement.innerText)
+            task.splice(busqueda, 1)
+            const item = e.parentElement.innerText
+            taskComplete.push(item)
+            localStorage.setItem("tareas", JSON.stringify(task))
+            localStorage.setItem("TareasComplete", JSON.stringify(taskComplete))
+
+        })
+    })
+}
+
+const removeTask = () => {
+    const btnRemove = document.querySelectorAll(".deletebtn")
+    btnRemove.forEach((e) => {
+        e.addEventListener('click', () => {
+            if (e.parentElement.className == "ready") {
+                const divItem = e.parentElement
+                const busqueda2 = taskComplete.indexOf(divItem.innerText)
+                taskComplete.splice(busqueda2, 1)
+                localStorage.setItem("TareasComplete", JSON.stringify(taskComplete))
+                e.parentElement.remove()
+            }
+            else {
+                const divItem = e.parentElement
+                const busqueda = task.indexOf(divItem.innerText)
+                task.splice(busqueda, 1)
+                localStorage.setItem("tareas", JSON.stringify(task))
+                e.parentElement.remove()
+            }
+
+
+        })
+    })
+}
+
+loadTask()
+tasksComplete()
+removeTask()
+add()
+
+
+
